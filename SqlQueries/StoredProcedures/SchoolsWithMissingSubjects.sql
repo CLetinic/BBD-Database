2c3be35f7d23964f3176c17196f5be521d6e740e
bbdnet2192 <susan@bbd.co.za>
2020-04-08 11:18:18 +0200

    Changes to comments, added new function

--- SqlQueries/StoredProcedures/SchoolsWithMissingSubjects.sql
+++ SqlQueries/StoredProcedures/SchoolsWithMissingSubjects.sql
@@ -1,23 +1,14 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
+-- ====================================================================
+-- Author:		Susan van Zyl
+-- Create date: 2020-04-08
+-- Description:	Get the schools who do not have educators registered 
+--              for all subjects
+-- Use (Ex) : EXEC SchoolsWithMissingSubjects
+-- ====================================================================
 CREATE PROCEDURE SchoolsWithMissingSubjects
 AS
 BEGIN
