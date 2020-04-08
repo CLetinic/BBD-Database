a1f237543d378ca6850b3c781004adc27df5c2a8
Meluleki Dube <dbxmel004@myuct.ac.za>
2020-04-08 18:42:41 +0200

    Added an error table for logging errors and error handle now logs errors to the table.

--- SqlQueries/Error_Handling/ErrorHandler.sql
+++ SqlQueries/Error_Handling/ErrorHandler.sql
@@ -1,14 +1,25 @@
 -- Verify that the stored procedure does not already exist.
-IF OBJECT_ID ( 'usp_GetErrorInfo', 'P' ) IS NOT NULL
+IF OBJECT_ID('usp_GetErrorInfo', 'P') IS NOT NULL
     DROP PROCEDURE usp_GetErrorInfo;
 GO
 
 -- Create procedure to retrieve error information.
 CREATE PROCEDURE usp_GetErrorInfo
 AS
-SELECT
-    ERROR_NUMBER() AS ErrorNumber
-     ,ERROR_PROCEDURE() AS ErrorProcedure
-     ,ERROR_LINE() AS ErrorLine
-     ,ERROR_MESSAGE() AS ErrorMessage;
+
+INSERT INTO dbo.DB_Errors
+VALUES (SUSER_SNAME(),
+        ERROR_NUMBER(),
+        ERROR_STATE(),
+        ERROR_SEVERITY(),
+        ERROR_LINE(),
+        ERROR_PROCEDURE(),
+        ERROR_MESSAGE(),
+        GETDATE());
+
+SELECT ERROR_NUMBER()    AS ErrorNumber
+     , ERROR_PROCEDURE() AS ErrorProcedure
+     , ERROR_LINE()      AS ErrorLine
+     , ERROR_MESSAGE()   AS ErrorMessage;
+
 GO
